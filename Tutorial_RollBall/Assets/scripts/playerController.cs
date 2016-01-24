8d9099db3eeb9863a4d313e81d9f3af9bc20c14f
lijeu <lijeu@hotmail.com>
2016-01-23 17:58:55 -0800

    Roll Ball Tutorial

---
+++ Tutorial_RollBall/Assets/scripts/playerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour {
+
+	public float movementSpeed;
+	public Text scoreText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int gameScore;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		gameScore = 0;
+		setGameScore ();
+		winText.text = "";
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * movementSpeed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup")) {
+			other.gameObject.SetActive (false);
+			gameScore = gameScore + 1;
+			setGameScore ();
+		}
+		//Destroy (other.gameObject);
+	}
+
+	void setGameScore()
+	{
+		scoreText.text = "Game Score: " + gameScore.ToString ();
+		if(gameScore >= 12){
+			winText.text = "You Win!";
+			scoreText.text = "";
+		}
+	}
+}
---
+++ Tutorial_RollBall/Assets/scripts/rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//.Rotate(x, y, z) applies a rotation around that point 
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
